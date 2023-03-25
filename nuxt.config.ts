ded3e745c11f5af0f9211de23ba830c7b810f212
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2023-03-25 15:11:06 -0300

    Add - Modules for css, icons and quasar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "nuxt-icon",
+        'nuxt-quasar-ui'
+    ],
 
-})
+    quasar: { /* */ },
+
+    
+});
