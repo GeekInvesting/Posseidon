4b4cc7be5566407537cdd35e1fb123b0dfde2b08
Heder Milani <hedermilani@hotmail.com>
2023-07-06 17:01:45 -0300

    Alterar base URL Hera

--- service/BaseUrl.ts
+++ service/BaseUrl.ts
@@ -2,5 +2,5 @@ const url = 'http://localhost'
 
 export const urlAtena = `${url}:80/atena`
 export const urlHefesto = `${url}:80/hefesto`
-export const urlHera = `${url}:8100/hera`
+export const urlHera = `${url}:80/hera`
 export const urlZeus = `${url}:8300/zeus`
